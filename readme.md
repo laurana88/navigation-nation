5adbf30ab8f76cee5d4daa41febee369ec8d6ebd
Laura Smith <laura.ann.smith88@gmail.com>
2021-11-16 16:05:09 -0500

    navigation

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Navigation Nation
+Seventh project for JavaScript Web Projects: 20 Projects to Build Your Portfolio
+
+The project focuses on creating an animated navigation that slides in and matches the color of the section it jumps to on this simple one-page website. This project utilizes Google Fonts, Unsplash Images, [W3 Schools Menu](https://www.w3schools.com/howto/howto_css_menu_icon.asp), and [Paletton color selector](https://paletton.com/).
+
+The main purpose of the project was to learn how to place and animate a navigation menu with CSS and Javascript.
+
+[Live URL](https://laurana88.github.io/navigation-nation/)
---
+++ script.js
@@ -0,0 +1,46 @@
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// control navigation animation
+const navAnimation = (directon1, direction2) => {
+    navItems.forEach((item, i) => {
+        item.classList.replace(`slide-${directon1}-${i+1}`, `slide-${direction2}-${i+1}`);
+    });
+}
+
+// main toggle function -> when you click the menu icon
+const toggleNav = () => {
+    // Toggle: menu bars open/closed
+    menuBars.classList.toggle('change');
+
+    // Toggle: menu active
+    overlay.classList.toggle('overlay-active');
+    if(overlay.classList.contains('overlay-active')) {
+
+        // Animate In Overlay
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+
+        // Animate - Nav Items
+        navAnimation('out', 'in');
+
+    } else {
+
+        //Animate Out Overlay
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+
+        // Animate - Nav Items
+        navAnimation('in', 'out');
+    }
+}
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+navItems.forEach((item) => {
+    item.addEventListener('click', toggleNav);
+});
